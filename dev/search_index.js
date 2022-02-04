5e9f57c10e4671e0d10c93217ad1e6bcb5cd2a7a
Documenter.jl <documenter@juliadocs.github.io>
2022-02-04 01:38:41 +0000

    build based on 5aaf4d3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OrnsteinZernike","category":"page"},{"location":"#OrnsteinZernike","page":"Home","title":"OrnsteinZernike","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OrnsteinZernike.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OrnsteinZernike]","category":"page"}]
+}
