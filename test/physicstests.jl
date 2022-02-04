8aba09fca865f378e8d74cee0855e335a35e7686
Edwin Bedolla <developeredwin@gmail.com>
2022-02-04 16:37:59 -0600

    :white_check_mark:
    Updating some of the test.

--- test/physicstests.jl
+++ test/physicstests.jl
@@ -1,6 +1,6 @@
 @testset "Results" begin
     rho = 0.8
-    nrho = 500
+    nrho = 50
     p = OrnsteinZernike.Parameters(1.0, 6.0, rho, 2^9, nrho, 1.0)
     pot = OrnsteinZernike.HardSphere()
     st = OrnsteinZernike.Structure(p, pot)
@@ -9,7 +9,11 @@
     inter = OrnsteinZernike.Interaction(p, st, cls)
 
     result = OrnsteinZernike.solve(inter)
-    first_contact = maximum(result.gr)
+    if 1.0 ∈ result.r
+        first_contact = result.gr[result.r .== 1.0][1]
+    else
+        first_contact = maximum(result.gr)
+    end
     ground_truth = 3.47 # For Percus-Yevick, this is the best it can get
     @show first_contact
     @test isapprox(first_contact, ground_truth; atol=0.01)
