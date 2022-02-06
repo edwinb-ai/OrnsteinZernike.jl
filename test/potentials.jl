6f085b388386851ee2b0d2e4c800c75e89a50cda
Edwin Bedolla <developeredwin@gmail.com>
2022-02-06 13:44:30 -0600

    :white_check_mark:
    This should include all tests of the current source code.
    Bumping version to v1.2

--- test/potentials.jl
+++ test/potentials.jl
@@ -41,3 +41,23 @@ end
     # We don't know exactly the value at contact, so just check that it is a valid number
     @test first_contact isa Real
 end
+
+@testset "Pseudo Hard Sphere" begin
+    rho = 0.8
+    nrho = 100
+    p = OrnsteinZernike.Parameters(1.0, 6.0, rho, 2^11, nrho, 1.0)
+    pot = OrnsteinZernike.PseudoHS()
+    st = OrnsteinZernike.Structure(p, pot)
+    cls = OrnsteinZernike.ModifiedVerlet()
+
+    inter = OrnsteinZernike.Interaction(p, st, cls)
+
+    result = OrnsteinZernike.solve(inter)
+    if 1.0 ∈ result.r
+        first_contact = result.gr[result.r .== 1.0][1]
+    else
+        first_contact = maximum(result.gr)
+    end
+    @show first_contact
+    @test first_contact isa Real
+end
